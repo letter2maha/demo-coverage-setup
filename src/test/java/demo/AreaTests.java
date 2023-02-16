1034a5371b107b2c73109a8ee38f76e5526f14f2
Maharaja <letter2maha@gmail.com>
2023-02-16 22:56:45 +0000

    without test2

--- src/test/java/demo/AreaTests.java
+++ src/test/java/demo/AreaTests.java
@@ -57,35 +57,5 @@ public class AreaTests {
     double expected = 0;
     Assert.assertEquals(0, Double.compare(expected, actual));
   }
-//  // Square
-//  @Test
-//  public void checkSquareAreaValidInput(){
-//    double s = 10;
-//    double actual = Area.square(s);
-//    double expected = 100;
-//    Assert.assertEquals(0, Double.compare(expected, actual));
-//  }
-//  @Test
-//  public void checkSquareAreaInvalidSideInput(){
-//    double s = -10;
-//    double actual = Area.square(s);
-//    double expected = 0;
-//    Assert.assertEquals(0, Double.compare(expected, actual));
-//  }
-  // Circle
-  @Test
-  public void checkCircleAreaValidInput(){
-    double radius = 10;
-    double expected = Math.PI * Math.pow(radius, 2);
-    double actual = Area.circle(radius);
-    Assert.assertEquals(0, Double.compare(expected, actual));
-  }
-  @Test
-  public void checkCircleAreaInvalidInput(){
-    double radius = -10;
-    double actual = Area.circle(radius);
-    double expected = 0;
-    Assert.assertEquals(0, Double.compare(expected, actual));
-  }
 
 }
